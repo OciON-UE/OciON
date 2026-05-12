deab0214262a9e48993141912d39e20e2de927c6
ldopacio <125046999+ldopacio@users.noreply.github.com>
2026-05-12 17:26:10 +0200

    Update generadorCupones.java
    dos métodos para generar cupones, dependiendo del tipo de cupon que se quiera usar

--- src/main/java/com/ocion/utils/generadorCupones.java
+++ src/main/java/com/ocion/utils/generadorCupones.java
@@ -1,5 +1,22 @@
 package com.ocion.utils;
 
+import java.time.LocalDate;
+
+import com.ocion.models.CantidadFija;
+import com.ocion.models.Cupon;
+import com.ocion.models.Descuento;
+
 public class generadorCupones {
+
+
+    public static Cupon generarCuponDescuento(String id, boolean canjeado, LocalDate fechaCreacion, LocalDate fechaExpiracion, double porcentaje) {
+        Cupon cuponDescuento = new Descuento(id, canjeado, fechaCreacion, fechaExpiracion, porcentaje);
+        return cuponDescuento;
+    }
+
+    public static Cupon generarCuponCantidadFija(String id, boolean canjeado, LocalDate fechaCreacion, LocalDate fechaExpiracion, double descCantidadFija) {
+        Cupon cuponCantFija = new CantidadFija(id, canjeado, fechaCreacion, fechaExpiracion, descCantidadFija);
+        return cuponCantFija;
+    }
 
 }
