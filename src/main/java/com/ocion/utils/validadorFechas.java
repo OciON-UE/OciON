a216a14cfae4915f11b3ca57bcd3668417d9fd67
ldopacio <125046999+ldopacio@users.noreply.github.com>
2026-05-12 17:31:05 +0200

    Update validadorFechas.java
    Poner el método que compara fecha con la actual para ver si el cupon o la oferta está caducado

--- src/main/java/com/ocion/utils/validadorFechas.java
+++ src/main/java/com/ocion/utils/validadorFechas.java
@@ -1,5 +1,18 @@
 package com.ocion.utils;
 
+import java.time.LocalDate;
+
 public class validadorFechas {
+
+    public static boolean estaCaducado (LocalDate fechaExpiracion) {
+        LocalDate fechaActual = LocalDate.now();
+        if (fechaActual.isAfter(fechaExpiracion) == true) {
+            System.out.println("La suscripción ha caducado.");
+            return true;
+        } else {
+            System.out.println("La suscripción sigue activa.");
+            return false;
+        }
+    }
 
 }
